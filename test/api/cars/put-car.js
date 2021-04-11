f56202a4af18363fe4859693b638a48d0c352807
AlexiaTdev <alexia.teissier1@epsi.fr>
2021-04-11 13:10:23 +0200

    put added to route testing, update of wrong routes /car that does not exist

--- test/api/cars/put-car.js
+++ test/api/cars/put-car.js
@@ -5,7 +5,7 @@ const CARS_DATA = require('../../data/cars');
 
 const gatewayUtils = require('../gateway.utils');
 
-describe('DELETE /cars/:id', function() {
+describe('PUT /cars/:id', function() {
   const collections = {};
 
   before(async () => {
@@ -32,22 +32,39 @@ describe('DELETE /cars/:id', function() {
    * https://medium.com/@Jscrambler/testing-apis-with-mocha-1d75bd4bcc0f
    * https://developer.mozilla.org/fr/docs/Web/HTTP/Status
    */
-  it('should delete a car', function(done) {
+  it('should make an update of a car', function(done) {
     const car = CARS_DATA['AA-768-RT'];
-    request.delete('/car/' + car._id)
-      .expect(201)
-      .end(function(err, res) {
-        done(err);
-      });
+    request.put('/cars/' + car._id)
+        .send({ _id: 'AA-768-RT', brand: 'Seat', model: 'Atecaaaaa' })
+        .expect(201)
+        .end(function(err, res) {
+            done(err);
+        });
   });
 
-  it('should NOT delete a car if the car does not exist', async () => {
+  it('should NOT update a car if the car does not exist', async () => {
     const car = { _id: 'A', brand: 'S', model: 'A' };
-    request.delete('/car/' + car._id)
-      .expect(400)
-      .end(function(err, res) {
-        done(err);
-      });
+    request.put('/cars/' + car._id)
+        .send(car)
+        .expect(400)
+        .end(function(err, res) {
+            done(err);
+        });
   });
 });
+
+
+/**
+ * describe('PUT /tasks/:id', function() {
+    it('updates a task', function(done) {
+      var task = app.db('tasks').first();
+      request.put('/tasks/' + task.id)
+        .send({ title: 'travel', done: false })
+        .expect(201)
+        .end(function(err, res) {
+          done(err);
+        });
+    });
+  });
+ */
 
